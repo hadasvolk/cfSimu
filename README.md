a8a993ab8e6a64887bb7a5239fad3d673be8ad0a
hadasvolk <hadasvol@pm.me>
2022-06-28 18:39:42 +0000

    full pipelline

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # cfSimu-wgsim
 Cell free simulation using Wgsim
+
+# Run
+1. copy hoobari's output maternal_lengths.csv and fetal_lengths.csv to cwd
+2. generate compressed fastqs with sim.py
+'''python 
+python /shared/home/hadas/identifai/repos/cfSimu-wgsim/sim.py --name fam50_maternal --n_seqs 1000000000 -nw 95 -o reads_maternal
+'''
+3. change Snakefile accordingly
+run
+'''python
+snakemae -c <n cores>
+'''
--- sim.py
+++ sim.py
@@ -36,7 +36,7 @@ def parseArgs() -> argparse.Namespace:
     parser.add_argument('--n_seqs', '-ns', help='Number of sequences', type=int, default=148576028, required=False)
     parser.add_argument('--n_workers', '-nw', help='Number of workers', type=int, default=1, required=False)
     parser.add_argument('--outdir', '-o', help='Output directory', type=str, default='simulations', required=False)
-    parser.add_argument('--fastq_list', '-fq', help='List of fastq files', type=str, default='fastq_list.txt', required=False)
+    parser.add_argument('--fastq_dict', '-fq', help='Pickle dict of fastq files produced', type=str, default='fastq_dict', required=False)
     return parser.parse_args()
 
 
@@ -47,50 +47,74 @@ class ReadGenerator:
         self.n_seqs = args.n_seqs
         self.ref = args.ref
         self.n_workers = args.n_workers
-        self.MAX_READS = 100000
+        self.MAX_READS = 1000000
         
         self.out = args.outdir
-        self.fastq_list = args.fastq_list
+        self.fastq_dict = args.fastq_dict
 
+        self.gendf()
+
+        self.fastq_yaml_path = os.path.join(self.out, "{}.yaml".format(self.fastq_dict))
+        self.fastq_tsv_path = os.path.join(self.out, "{}.tsv".format(self.fastq_dict))
+        self.read_dict_path = os.path.join(self.out, "{}.pkl".format(self.fastq_dict))
+        self.read_dict_exists = os.path.exists(self.read_dict_path)
+        self.records_pkl_path =  os.path.join(self.out, '{}.all_records.pkl'.format(self.name))
+        self.records_pkl_exists = os.path.exists(self.records_pkl_path)
+        if self.records_pkl_exists and not self.read_dict_exists:
+            with open(self.records_pkl_path, 'rb') as f:
+                self.all_records  = pickle.load(f)
+                self.pkl_read_dict()
+        elif self.read_dict_exists:
+            with open(self.read_dict_path, 'rb') as f:
+                self.fastq_dict = pickle.load(f)
+        else:
+            self.genreads()
+
+    def genreads(self):
         try:
             os.makedirs(self.out, exist_ok=True)
         except OSError:
             print("Error: Creating output directory")
             exit()
 
-        self.gendf()
-
-        all_records = []
-        all_records_pkl = os.path.join(self.out, '{}.all_records.pkl'.format(self.name))
-        if os.path.exists(all_records_pkl):
-            with open(all_records_pkl, 'rb') as f:
-                all_records = pickle.load(f)
-        else:
-            for l in self.lengths_df['length'].unique():
-                i = 0
-                mod = divmod(self.lengths_df[self.lengths_df['length'] == l]['n_reads'].values[0], self.MAX_READS)
-                if mod[0] != 0:
-                    for i in range(mod[0]):
-                        all_records.append([l, self.MAX_READS, i])
-                    all_records.append([l, mod[1], i+1])
-                else:
-                    all_records.append([l, mod[1], 0])
-            pickle.dump(all_records, open(all_records_pkl, 'wb'))
-        print("number of records:",len(all_records))
-        str_all_rec = [','.join(map(str, l)) for l in all_records]
+        self.all_records = []
+        for l in self.lengths_df['length'].unique():
+            i = 0
+            mod = divmod(self.lengths_df[self.lengths_df['length'] == l]['n_reads'].values[0], self.MAX_READS)
+            if mod[0] != 0:
+                for i in range(mod[0]):
+                    self.all_records .append([l, self.MAX_READS, i])
+                self.all_records.append([l, mod[1], i+1])
+            else:
+                self.all_records .append([l, mod[1], 0])
+        pickle.dump(self.all_records , open(self.records_pkl_path, 'wb'))
+        print("number of records:",len(self.all_records ))
+        str_all_rec = [','.join(map(str, l)) for l in self.all_records ]
         with Pool(processes = self.n_workers) as pool:
             pool.map(self.exec, str_all_rec)
-        # for l in str_all_rec:
-        #     self.exec(l)
+        
+        self.pkl_read_dict()
 
-        a = {}
-        for record in all_records:
+    def pkl_read_dict(self):    
+        self.fastq_dict = {}
+        self.fastq_yaml = []
+        self.fastq_tsv = []
+        for record in self.all_records :
+            name = "{}-{}".format(record[0], record[2])
             r1 = "{}/read1.{}.{}.fq.gz".format(self.out, record[0], record[2])
             r2 = "{}/read2.{}.{}.fq.gz".format(self.out, record[0], record[2])
-            a["{}-{}".format(record[0], record[2])] = [r1, r2] 
-        print("number of records:",len(all_records))
-        with open(os.path.join(self.out, self.fastq_list), 'wb') as handle:
-            pickle.dump(all_records, handle)
+            self.fastq_dict[name] = [r1, r2]
+            self.fastq_yaml.append({'name': name, 'read1': r1, 'read2': r2})
+            self.fastq_tsv.append([name, r1, r2])
+        print("number of records:",len(self.all_records ))
+        
+        with open(self.fastq_yaml_path, 'w') as yf:
+            yaml.dump({'samples':self.fastq_yaml}, yf, sort_keys=False, default_flow_style=False)
+        
+        with open(self.read_dict_path, 'wb') as handle:
+            pickle.dump(self.fastq_dict, handle)
+        
+        pd.DataFrame(self.fastq_tsv, columns=['name', 'read1', 'read2']).to_csv(self.fastq_tsv_path, sep='\t', index=False)
 
 
     def gendf(self) -> None:
@@ -160,6 +184,12 @@ class ReadGenerator:
     def write_fastq_compress(self, seqs):
         with bgzf.BgzfWriter("{}.gz".format(seqs[0]), "wb") as outgz:
             SeqIO.write(sequences=seqs[1], handle=outgz, format="fastq")
+    
+    def aligncmds(self):
+        cmds = []
+        for i in self.fastq_dict.keys():
+            cmds.append("{} {} {}".format(self.aligner, self.fastq_dict[i][0], self.fastq_dict[i][1]))
+        return cmds
 
 
 if __name__ == '__main__':   
